60dd1945b549c1552778d5e2badc580164aeb6de
LIYI-Git <2573012202@qq.com>
2019-04-25 18:54:17 +0800

    提交修改main方法

--- SpringBootDemo/src/test/java/com/maven/TestDemo.java
+++ SpringBootDemo/src/test/java/com/maven/TestDemo.java
@@ -4,4 +4,7 @@ public class TestDemo {
 	
 	private static ThreadLocal<Integer> loacl = new ThreadLocal<Integer>();
 
+	public static void main(String[] args) {
+		System.out.println("hello ");
+	}
 }
